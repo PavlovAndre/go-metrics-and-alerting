fa8981d7d7ecb199b90d7a367ff9ecd2d6c793b3
PavAndr <pavl0vandrei@mail.ru>
2025-07-22 02:35:17 +0500

    7 iter

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -247,7 +247,7 @@ func ValueJSON(store *repository.MemStore) http.HandlerFunc {
 			if err != nil {
 				log.Printf("Error marshalling json: %s\n", err)
 			}
-			if _, err := fmt.Fprint(w, "application/json", body); err != nil {
+			if _, err := fmt.Fprint(w, " Content-Type: application/json", body); err != nil {
 				log.Printf("Failed to ValueJson: %v", err)
 				w.WriteHeader(http.StatusInternalServerError)
 				return
@@ -269,7 +269,7 @@ func ValueJSON(store *repository.MemStore) http.HandlerFunc {
 			if err != nil {
 				log.Printf("Error marshalling json: %s\n", err)
 			}
-			if _, err := fmt.Fprint(w, "application/json", body); err != nil {
+			if _, err := fmt.Fprint(w, " Content-Type: application/json", body); err != nil {
 				log.Printf("Failed to ValueJson: %v", err)
 				w.WriteHeader(http.StatusInternalServerError)
 				return
