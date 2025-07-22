95902d54bd8ba0f93d38be42e1a845201a8fdebd
PavAndr <pavl0vandrei@mail.ru>
2025-07-22 16:17:43 +0500

    7 iter

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -186,8 +186,8 @@ func UpdateJSON(store *repository.MemStore) http.HandlerFunc {
 				return
 			}
 			store.SetGauge(req.ID, *req.Value)
-			//w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusCreated)
 		}
 
 		//Выполняем инкремент значения counter
